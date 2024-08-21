b3e42c5146ce112994661baf89d21d4379502fbb
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-08-21 23:59:07 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,11 @@
+def test_function():
+    def innter_funtion ():
+        print('я в области функции test_function')
+        innter_funtion()
+        return
+
+
+ a= 5
+ b= test_function(4)
+ print(a)
+ print(b)
